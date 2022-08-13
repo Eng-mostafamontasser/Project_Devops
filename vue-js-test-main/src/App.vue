5b6edc789fe282da10f4685b34e2281b635b2a5f
Eng-mostafamontasser <mostafamontasser66@gmail.com>
2022-08-13 18:42:27 +0200

    add repo devops

---
+++ vue-js-test-main/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <Header title="Task Tracker" />
+    </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+    name: 'App',
+    components: {
+        Header,
+    },
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+.container {
+    max-width: 500px;
+    margin: 30px auto;
+    overflow: auto;
+    min-height: 300px;
+    border: 1px solid steelblue;
+    padding: 30px;
+    border-radius: 5px;
+}
+
+</style>
